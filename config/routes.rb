adbde929ec0d1bb15245f3761b7f20565b21c054
Luan Nguyen Thanh <tklarryonline@gmail.com>
2013-09-19 13:43:06 +0700

    [f] 2A - Fill in the layout for three pages [https://trello.com/c/N4gbqJl5]
    - Separate layout into partials
    - Fill in the layout for Home page with call to actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Sp2usMgmt::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root 'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   resources :posts
 
@@ -49,7 +50,7 @@ Sp2usMgmt::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,29 +2,32 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Welcome') }
+    it { should have_title(full_title('')) }
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Welcome to Space++')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
       expect(page).to have_title("Space++ Management")
       expect(page).not_to have_title("Home")
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
       expect(page).to have_title("Help")
     end
   end
@@ -32,13 +35,26 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit contact_path
+      expect(page).to have_title("Contact")
     end
   end
 end
