bb069a022b8a77fdf53dbabd28abd3ff76d8e19b
Luan Nguyen Thanh <tklarryonline@gmail.com>
2013-09-19 18:32:49 +0700

    [r] Add application helper spec test

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,2 @@
-def full_title(page_title)
-  base_title = "Space++ Management"
-  if page_title.empty?
-    base_title
-  else
-    "#{page_title} | #{base_title}"
-  end
-end
+include ApplicationHelper
+
