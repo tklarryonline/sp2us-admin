f38f7e714df9032b2ad964fea0e07fe7b34ab1d5
Luan Nguyen Thanh <tklarryonline@gmail.com>
2013-09-22 03:01:03 +0700

    [r] Moved admins' layout files to layouts/admin/

--- config/initializers/active_admin_patch.rb
+++ config/initializers/active_admin_patch.rb
@@ -2,7 +2,7 @@ module ActiveAdmin
   class BaseController
     helper ActiveAdminHelper
     def determine_active_admin_layout
-      'active_admin_reskin'
+      'admin/active_admin_reskin'
     end
   end
 end
