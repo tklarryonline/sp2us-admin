e167be76c58dc7f9e4a9c541a679dc703e56dcf6
Luan Nguyen Thanh <tklarryonline@gmail.com>
2013-09-24 12:28:18 +0700

    [r] Added database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# ruby encoding: utf-8
+# AdminUser
+AdminUser.delete_all
+
+AdminUser.create(email: "admin@example.com", password: "password", password_confirmation: "password")
+AdminUser.create(email: "luan@tklarryonline.me", password: "password", password_confirmation: "password")
+AdminUser.create(email: "vux@sp2us.com", password: "password", password_confirmation: "password")
+
+# Rooms
+Room.delete_all
+
+Room.create(name: "Brainstorming Room", price_per_hour: 4.0, capacity: 10)
+Room.create(name: "Creative Room", price_per_hour: 3.0, capacity: 7)
+Room.create(name: "Workshop Room", price_per_hour: 6.0, capacity: 15)
+
+# User
+User.delete_all
+
+User.create(name: "TK Larry", email: "tklarryonline@gmail.com", password: "password", password_confirmation: "password")
+User.create(name: "Luan Nguyen Thanh", email: "luan.nguyenthanh@rmitc.org", password: "password", password_confirmation: "password")
+User.create(name: "Vux Nguyen", email: "vuxnguyenonline@gmail.com", password: "password", password_confirmation: "password")
